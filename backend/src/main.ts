4a09ca2b5ea623ccd9bb7fbc0e7e12ca4e1d10d3
unknown <weedkhalifa1608@gmail.com>
2025-12-08 20:50:51 +0900

    updat3

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors(process.env.CORS_ORIGIN ? { origin: process.env.CORS_ORIGIN } : undefined);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
